fb394fe83a3487f549a75e9c1369b841cbd063c2
Nick Jones <jonesn5@miamioh.edu>
2024-06-25 13:57:54 -0400

    Added profile and profile mapper.

---
+++ src/main/java/com/cooksys/socialmedia/entities/Profile.java
@@ -0,0 +1,21 @@
+package com.cooksys.socialmedia.entities;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Profile {
+
+	private String firstName;
+	
+	private String lastName;
+	
+	private String email;
+	
+	private String phone;
+}
