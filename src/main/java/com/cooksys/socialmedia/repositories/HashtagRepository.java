24eefebc51d7529c607237fa076d394c56a14ce2
Helena Makendengue <HelenaMakens1013@gmail.com>
2024-06-25 14:50:46 -0500

    Update HashtagRepository.java

--- src/main/java/com/cooksys/socialmedia/repositories/HashtagRepository.java
+++ src/main/java/com/cooksys/socialmedia/repositories/HashtagRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cooksys.socialmedia.entities.Hashtag;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface HashtagRepository extends JpaRepository<Hashtag, Long>{
 
 }
