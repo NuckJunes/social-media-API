7495e6cc91234153c3ecee26a6a24987c3d0483e
Helena Makendengue <HelenaMakens1013@gmail.com>
2024-06-25 14:53:14 -0500

    Update NotAuthorizedException.java

--- src/main/java/com/cooksys/socialmedia/exceptions/NotAuthorizedException.java
+++ src/main/java/com/cooksys/socialmedia/exceptions/NotAuthorizedException.java
@@ -1,5 +1,12 @@
 package com.cooksys.socialmedia.exceptions;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
 public class NotAuthorizedException extends RuntimeException {
 
 	/**
